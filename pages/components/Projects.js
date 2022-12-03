c19d28ea17c31d5c97bfb7e836e0be88521cc851
afrith03 <afrithshariff20@gmail.com>
2022-12-04 00:48:14 +0530

    bug fixes

--- pages/components/Projects.js
+++ pages/components/Projects.js
@@ -1,7 +1,51 @@
 import React from "react";
-import { ProjectArray } from "./projectsJson";
 import { motion } from "framer-motion";
 function Projects({ scaleEffect }) {
+
+  const ProjectArray = [
+    {
+      id: 1,
+      title: "Myth's Player",
+      description:
+        "Myth's player is a modern, lightweight web music player that combines the flexibility of HTML, CSS, and JavaScript. also supports features such as shuffle, repeat, and more.",
+      link: "https://afrith03.github.io/musicplayer/",
+      image: "url(../public/music-player.png)",
+    },
+    {
+      id: 2,
+      title: "Al-Miraaj.com",
+      description:
+        "Almiraj.com was a great client to work with. I was able to help them create a stunning landing page that was designed to engage visitors and generate leads. the response to the landing page has been overwhelmingly positive.",
+      link: "https://almiraaj.com",
+      image: "url(../public/almiraaj.png)",
+    },
+    // {
+    //   id: 3,
+    //   title: "Afrith.js",
+    //   description:
+    //     "afrith.js is a lightweight, easy-to-use npm package. It will make you console look a little colourful Check this out if you are a fan of 'Vaigai puyal'",
+    //   link: "https://socket.dev/npm/package/afrith",
+    //   image: "url(../public/afrith-js.png)",
+    // },
+    {
+      id: 3,
+      title: "News Monkey",
+      description:
+        "NewsMonkey is a React app that gives you real-time news from anywhere in the world. It is configured with Newsapi to fetch news. You can also customize the news feed to get the news that you want.",
+      link: "https://github.com/afrith03/NewsMonkey",
+      image: "url(../public/news-monkey.png)",
+    },
+    {
+      id: 4,
+      title: "Text Utils",
+      description:
+        "TextUtils is a React.js application to format your text by Capitalizing and other formatting options. It provides features like word count and time takes to read the paragraph, It is an open-source application",
+      link: "https://text-utils-9fb3.netlify.app/",
+      image: "url(../public/text-utils.png)",
+    },
+  ];
+  
+
   return (
     // project 1
     <div className="text-slate-700 webkit">
